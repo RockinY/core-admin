5c2b994b987d7843fa7400287a7df49a320f02b9
Bran Liang <lby89757@hotmail.com>
2018-10-30 22:10:55 +0800

    remove atlaskit

--- src/routes.js
+++ src/routes.js
@@ -2,61 +2,16 @@ import React, { Component } from 'react'
 import { Router, Route, Switch } from 'react-router'
 import Dashboard from './views/dashboard'
 import createBrowserHistory from 'history/createBrowserHistory'
-import {
-  NavigationProvider,
-  LayoutManager,
-  Item,
-  Section
-} from '@atlaskit/navigation-next';
-import GlobalNavigation from '@atlaskit/global-navigation';
-import DashboardIcon from '@atlaskit/icon/glyph/dashboard';
-import { Logo } from './components/logo'
-import {
-  ProductNavigationWrapper,
-  PageWrapper
-} from './styles'
 
 const history = createBrowserHistory()
-
-/* Global Navigation */
-const CustomGlobalNavigation = () => (
-  <GlobalNavigation
-    productIcon={Logo}
-    productTooltip='云社'
-    onProductClick={() => {}}
-  />
-);
-
-/* Product Navigation */
-const CustomProductNavigation = () => (
-  <ProductNavigationWrapper>
-    <Section>
-      {({ className }) => (
-        <div className={className}>
-          <Item before={DashboardIcon} text="概况" />
-        </div>
-      )}
-    </Section>
-  </ProductNavigationWrapper>
-)
 
 class Routes extends Component {
   render () {
     return (
       <Router history={history}>
-        <NavigationProvider>
-          <LayoutManager
-            globalNavigation={CustomGlobalNavigation}
-            productNavigation={CustomProductNavigation}
-            containerNavigation={null}
-          >
-            <PageWrapper>
-              <Switch>
-                <Route exact path='/' component={Dashboard} />
-              </Switch>
-            </PageWrapper>
-          </LayoutManager>
-        </NavigationProvider>
+        <Switch>
+          <Route exact path='/' component={Dashboard} />
+        </Switch>
       </Router>
     )
   }
--- src/views/dashboard/index.js
+++ src/views/dashboard/index.js
@@ -1,26 +1,14 @@
 import React, { Component } from 'react'
 import { Query } from 'react-apollo'
-import { BreadcrumbsStateless, BreadcrumbsItem } from '@atlaskit/breadcrumbs'
-import PageHeader from '@atlaskit/page-header'
 import { META_INFORMATION_QUERY } from './queries'
-
-const breadcrumbs = (
-  <BreadcrumbsStateless onExpand={() => {}}>
-    <BreadcrumbsItem text="概况" key="dashboard" />
-  </BreadcrumbsStateless>
-)
 
 class Dashboard extends Component {
   render () {
     return (
       <React.Fragment>
-        <PageHeader breadcrumbs={breadcrumbs}>
-          主要数据指标
-        </PageHeader>
         <Query query={META_INFORMATION_QUERY}>
           {({ loading, error, data }) => {
-            console.log(data);
-            return null
+            return data
           }}
         </Query>
       </React.Fragment>
