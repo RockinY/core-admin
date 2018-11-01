1c4ac81bc5e83d78c62a6ee8cbd30fa5714ba325
Bran Liang <lby89757@hotmail.com>
2018-11-01 18:33:52 +0800

    enable dynamic link tab

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -7,11 +7,13 @@ import {
 
 class TopBar extends Component {
   render() {
+    const pathname = this.props.location.pathname
+
     return (
       <NavWrapper>
         <NavItemWrapper>
-          <NavItem to='/' active>后台首页</NavItem>
-          <NavItem to='/users'>用户列表</NavItem>
+          <NavItem to='/' active={pathname === '/' ? 1 : 0}>后台首页</NavItem>
+          <NavItem to='/users' active={pathname === '/users' ? 1 : 0}>用户列表</NavItem>
         </NavItemWrapper>
       </NavWrapper>
     )
